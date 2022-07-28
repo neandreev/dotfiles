e95d89a82b49b4c8fa9af959270da4543c9be397
Danila Andreev <mail@neandreev.ru>
2022-07-28 21:55:40 +0500

    fix default nvim-notify background colour in config

--- .config/nvim/lua/configs/notify.lua
+++ .config/nvim/lua/configs/notify.lua
@@ -1,4 +1,4 @@
 local status_ok, notify = pcall(require, "notify")
 if not status_ok then return end
-notify.setup(astronvim.user_plugin_opts("plugins.notify", { stages = "fade" }))
+notify.setup(astronvim.user_plugin_opts("plugins.notify", { background_colour = "#000000", stages = "fade" }))
 vim.notify = notify
